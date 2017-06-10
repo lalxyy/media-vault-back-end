2cc4744550de275fc234f5e9db23c738fd6fb06e
Carl Li <hkyla00@126.com>
2017-06-10 15:41:33 +0800

    Initial commit

---
+++ src/main/java/project/mediavault/MediaVaultApplication.java
@@ -0,0 +1,15 @@
+package project.mediavault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class MediaVaultApplication {
+
+    public static void main(String[] args) {
+		SpringApplication.run(MediaVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/mediavault/MediaVaultApplicationTests.java
@@ -0,0 +1,16 @@
+package project.mediavault;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MediaVaultApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
