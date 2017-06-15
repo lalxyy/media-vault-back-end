53fb958b568c9f1584309e90c724a16b3ae61aac
Carl Li <hkyla00@126.com>
2017-06-15 22:08:42 +0800

    add static files assignment 1

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # Media Vault (Back end)
 
+A Spring Boot Project (Maven).
 
+## Setup
+
+Create MySQL database 'mediavault' at localhost as below; or the program would fail.
+
+```
+jdbc:mysql://localhost/mediavault
+Username: mediavault
+Password: []mv0
+```
+
+## Compiling & Packaging
+
+``` bash
+# directly run without external tomcat
+mvn spring-boot:run
+```
+
+Just use the given .war in the root.
+
+Or create .war manually: `mvn package`.
--- src/main/java/project/mediavault/controller/FileUploadController.java
+++ src/main/java/project/mediavault/controller/FileUploadController.java
@@ -3,10 +3,7 @@ package project.mediavault.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -29,6 +26,7 @@ public class FileUploadController {
         return new BigInteger(130, random).toString(32);
     }
 
+    @CrossOrigin(origins = "http://localhost:8377")
     @RequestMapping("/api/files/add")
     public ResponseEntity<ModelMap> uploadFile(@RequestParam("file") MultipartFile file) {
         ModelMap modelMap = new ModelMap("success", true)
