86edefeef89698baf890b4c9aad369f7c24a8078
Carl Li <hkyla00@126.com>
2017-06-15 17:17:51 +0800

    file upload

---
+++ src/main/java/project/mediavault/controller/FileUploadController.java
@@ -0,0 +1,48 @@
+package project.mediavault.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+/**
+ * File Upload Controller.
+ *
+ * @author Carl Li
+ */
+@RestController
+//@RequestMapping("/api/files")
+public class FileUploadController {
+
+    private SecureRandom random = new SecureRandom();
+
+    private String fileNameRandomSeed() {
+        return new BigInteger(130, random).toString(32);
+    }
+
+    @RequestMapping("/api/files/add")
+    public ResponseEntity<ModelMap> uploadFile(@RequestParam("file") MultipartFile file) {
+        ModelMap modelMap = new ModelMap("success", true)
+                .addAttribute("fileURL", "/files/" + fileNameRandomSeed() + "-" + file.getOriginalFilename());
+        return new ResponseEntity<>(modelMap, HttpStatus.OK);
+
+//        try (RandomAccessFile randomAccessFile = new RandomAccessFile(file.getOriginalFilename() + fileNameRandomSeed(), "rw")) {
+//            //
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            modelMap.addAttribute("success", "false");
+//            modelMap.addAttribute("error", "io failed");
+//            return new ResponseEntity<>(modelMap, HttpStatus.BAD_REQUEST);
+//        }
+    }
+
+}
