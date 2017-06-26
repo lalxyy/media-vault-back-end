e19a6b91d6d48ac3c7882f3977a1a5a255b79412
Carl Li <hkyla00@126.com>
2017-06-26 15:51:30 +0800

    add external resource mapping

--- src/main/java/project/mediavault/MediaVaultApplication.java
+++ src/main/java/project/mediavault/MediaVaultApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -45,6 +46,22 @@ public class MediaVaultApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**");
+			}
+		};
+	}
+
+	@Bean
+	public WebMvcConfigurer staticResourceConfigurer() {
+    	return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addResourceHandlers(ResourceHandlerRegistry registry) {
+				registry.addResourceHandler("/**")
+						.addResourceLocations("classpath:/META-INF/resources/")
+						.addResourceLocations("classpath:/resources/")
+						.addResourceLocations("classpath:/static/")
+						.addResourceLocations("classpath:/public/");
+				registry.addResourceHandler("/user-data/**")
+						.addResourceLocations("file:" + MediaVaultApplication.BASE_DIR + "/data/");
 			}
 		};
 	}
